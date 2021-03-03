d3e48b1225045542dbfa109fee1ac2f1347849dd
Caden Choi <cadenc.7@gmail.com>
2021-03-03 20:39:23 +0000

    works

---
+++ middle.js
@@ -0,0 +1,40 @@
+let eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const middle = function(arr1) {
+  if(arr1.length <= 2) {
+    console.log("no middle");
+    return [];
+  } else if(arr1.length % 2 === 0) {
+    console.log("even");
+    var middle = [arr1[(arr1.length)/2 -1], arr1[(arr1.length/2)]];
+    return middle;
+  } else {
+    console.log("odd");
+    var oddMiddle = [Math.floor((arr1.length)/2) +1];
+    return oddMiddle;
+  }
+}
+
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => [])
+console.log(middle([1, 2, 3])) // => [2]
+console.log(middle([1, 2, 3, 4, 5]) )// => [3])
+console.log(middle([1, 2, 3, 4]) );// => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]);
