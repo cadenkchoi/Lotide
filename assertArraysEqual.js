c0b2efd8056afe2760112de7c5dc25cf90ac0797
Caden Choi <cadenc.7@gmail.com>
2021-03-03 03:19:50 +0000

    lotide

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+let eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+/* Test Code
+assertArraysEqual([1, 2, 3], [1, 2, 3]);// => true
+assertArraysEqual([1, 2, 3], [0, 2, 1]);// => false
+*/
