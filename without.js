e20ba7d8d4d1b8575daf07a7ef3843272315417c
Caden Choi <cadenc.7@gmail.com>
2021-03-03 03:48:19 +0000

    d2 complete

---
+++ without.js
@@ -0,0 +1,35 @@
+let eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const without = function(arr, exclude) {
+  for(i = 0; i < arr.length; i++) {
+    for (x = 0; x < exclude.length; x++) {
+        if (exclude.includes(arr[i])) {
+          arr.splice(i,1);
+        }
+    }
+  }
+  return arr
+};
+
+without([1, 2, 3], [1]) // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+
+
+
