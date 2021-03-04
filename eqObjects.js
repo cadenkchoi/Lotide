91fc63abfd09e2e668482001af3aee898869b1d7
Caden Choi <cadenc.7@gmail.com>
2021-03-04 07:34:07 +0000

    objects is unfinished, small change to arrays

---
+++ eqObjects.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// const eqArrays = function(arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       return false;
+//     }
+//   } return true;
+// };
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length != Object.keys(object2).length) {
+    return false;
+  }
+}
+  // for (const key in object1) {
+  //   if (Array.isArray(object1[key]))
+  // }
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log(eqObjects(cd, dc)); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+console.log(eqObjects(cd, cd2)); // => false
