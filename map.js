9bc141ff66ffa1d3b94b423b5b5e72a6496e9406
Caden Choi <cadenc.7@gmail.com>
2021-03-05 00:00:03 +0000

    map function with test using assertArraysEqual

---
+++ map.js
@@ -0,0 +1,33 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+const words2 = ["ground", "control", "tom", "major", "to"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+const results1 = map(words, word => word[0]);
+// console.log(results1);
+
+assertArraysEqual(eqArrays(words, words2), false);
