aa1a0beabb0db8c6231d31ef82999922e33a3062
Caden Choi <cadenc.7@gmail.com>
2021-03-05 05:14:09 +0000

    finally got this done...

--- eqObjects.js
+++ eqObjects.js
@@ -6,29 +6,43 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// const eqArrays = function(arr1, arr2) {
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
-//   for (let i = 0; i < arr1.length; i++) {
-//     if (arr1[i] !== arr2[i]) {
-//       return false;
-//     }
-//   } return true;
-// };
-
-const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length != Object.keys(object2).length) {
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
     return false;
   }
-}
-  // for (const key in object1) {
-  //   if (Array.isArray(object1[key]))
-  // }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const eqObjects = function(object1, object2) {
+  let objKey1 = Object.keys(object1);
+  let objKey2 = Object.keys(object2);
+  if (objKey1.length !== objKey2.length) {
+    return false;
+  }
+  for (const key in object1) {
+    if (Array.isArray(object1[key]) && (Array.isArray(object2[key]))) {
+      if ((eqArrays(object1[key], object2[key])) !== true) {
+        return false;
+      }
+    }
+    else if (object1[key] !== object2[key]) { 
+      return false;
+     }
+  }
+  return true;
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true); // => true
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
+assertEqual(eqObjects(cd, dc), false); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
+assertEqual(eqObjects(cd, cd2), false); // => false
